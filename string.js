b6f3560edbc650706baf24ce681b4925c8286fca
VasuGargDev <96354306+VasuGargDev@users.noreply.github.com>
2023-02-28 09:42:59 +0530

    string.js

---
+++ string.js
@@ -0,0 +1,8 @@
+let s = "KG Coding Youtube Subscribe Youtube";
+console.log(s.length);
+console.log(s.indexOf("Youtube"));
+console.log(s.lastIndexOf("Youtube"));
+console.log(s.slice(0,5));   //include first exclude last
+
+s1= s.replace("Youtube","Instagram");
+console.log(s1);
